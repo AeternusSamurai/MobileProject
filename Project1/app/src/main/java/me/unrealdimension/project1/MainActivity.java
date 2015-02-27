90de7b3eb4f157ffdc1151834baf13b7476086d8
AeternusSamurai <darksamurai515@gmail.com>
2015-02-27 14:17:01 -0500

    Another test of the Git integration

--- Project1/app/src/main/java/me/unrealdimension/project1/MainActivity.java
+++ Project1/app/src/main/java/me/unrealdimension/project1/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String temp = "I'm a string";
+        String temp = "This is another test";
     }
 
 
