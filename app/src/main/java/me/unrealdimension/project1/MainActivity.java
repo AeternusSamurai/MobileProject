2fb9dcd841436a7e778195e2e545f300e0f378e9
AeternusSamurai <darksamurai515@gmail.com>
2015-04-08 17:50:59 -0400

    Testing the code found online

--- app/src/main/java/me/unrealdimension/project1/MainActivity.java
+++ app/src/main/java/me/unrealdimension/project1/MainActivity.java
@@ -1,23 +1,63 @@
 package me.unrealdimension.project1;
 
 
+/*
+Importing Android Class And Google FIT API
+ */
+import android.app.ActionBar;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.SystemClock;
-import android.support.v7.app.ActionBarActivity;
+import android.content.IntentSender;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DataSourcesResult;
+import java.util.concurrent.TimeUnit;
 
 
-public class MainActivity extends ActionBarActivity {
-    private Chronometer mChronometer;
-    long stopTime = 0;
-    long baseTime = SystemClock.elapsedRealtime()-stopTime; // The initial base time for when the timer is first started.
-    long elapsedTime;
+public class MainActivity extends ActionBarActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "FitActivity";
+    //[START Auth_Variable_References]
+    private static final int REQUEST_OAUTH = 1;
+    // [END auth_variable_references]
+    private GoogleApiClient mClient = null;
+
+    int mInitialNumberOfSteps = 0;
+    private TextView mStepsTextView;
+    private boolean mFirstCount = true;
+
+    // Create Builder View
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStepsTextView = (TextView) findViewById(R.id.step_counter);
+    }
 
     @Override
     public void onBackPressed() {
@@ -40,21 +80,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //mChronometer = (Chronometer)findViewById(R.id.chronometer);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_info:
@@ -73,25 +98,173 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public void startTimer(View v) {
-        // TODO use setBase method of the chronometer the restart it from the last time that it was started from
-        //baseTime = SystemClock.elapsedRealtime() - stopTime;
-        mChronometer.setBase(baseTime);
-        mChronometer.start();
+    private void connectFitness() {
+        Log.i(TAG, "Connecting...");
+
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
+                // select the Fitness API
+                .addApi(Fitness.SENSORS_API)    // Required only if you're making SensorsApi calls
+                .addApi(Fitness.RECORDING_API)  // Required only if you're making RecordingApi calls
+                .addApi(Fitness.HISTORY_API)    // Required only if you're making HistoryApi calls
+                .addApi(Fitness.SESSIONS_API)
+                        // specify the scopes of access
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
+                        // provide callbacks
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        // Connect the Google API client
+        mClient.connect();
     }
 
-    public void resetTimer(View v) {
-        mChronometer.stop();
-        baseTime = 0;
-        stopTime = 0;
-        mChronometer.setBase(0);
+    // Manage OAuth authentication
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+        // Error while connecting. Try to resolve using the pending intent returned.
+        if (result.getErrorCode() == ConnectionResult.SIGN_IN_REQUIRED ||
+                result.getErrorCode() == FitnessStatusCodes.NEEDS_OAUTH_PERMISSIONS) {
+            try {
+                // Request authentication
+                result.startResolutionForResult(this, REQUEST_OAUTH);
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, "Exception connecting to the fitness service", e);
+            }
+        } else {
+            Log.e(TAG, "Unknown connection issue. Code = " + result.getErrorCode());
+        }
     }
 
-    public void stopTimer(View v) {
-        mChronometer.stop();
-        //baseTime = SystemClock.elapsedRealtime() - stopTime;
-        //stopTime = Long.parseLong(mChronometer.getText().toString());
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_OAUTH) {
+            if (resultCode == RESULT_OK) {
+                // If the user authenticated, try to connect again
+                mClient.connect();
+            }
+        }
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+        // If your connection gets lost at some point,
+        // you'll be able to determine the reason and react to it here.
+        if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+            Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+        } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+            Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Log.i(TAG, "Connected!");
+
+        // Now you can make calls to the Fitness APIs.
+        invokeFitnessAPIs();
+
+    }
+
+    private void invokeFitnessAPIs() {
+
+        // Create a listener object to be called when new data is available
+        OnDataPointListener listener = new OnDataPointListener() {
+            @Override
+            public void onDataPoint(DataPoint dataPoint) {
+
+                for (Field field : dataPoint.getDataType().getFields()) {
+                    Value val = dataPoint.getValue(field);
+                    updateTextViewWithStepCounter(val.asInt());
+                }
+            }
+        };
+
+        //Specify what data sources to return
+        DataSourcesRequest req = new DataSourcesRequest.Builder()
+                .setDataSourceTypes(DataSource.TYPE_DERIVED)
+                .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA)
+                .build();
+
+        //  Invoke the Sensors API with:
+        // - The Google API client object
+        // - The data sources request object
+        PendingResult<DataSourcesResult> pendingResult =
+                Fitness.SensorsApi.findDataSources(mClient, req);
+
+        //  Build a sensor registration request object
+        SensorRequest sensorRequest = new SensorRequest.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .setSamplingRate(1, TimeUnit.SECONDS)
+                .build();
+
+        //  Invoke the Sensors API with:
+        // - The Google API client object
+        // - The sensor registration request object
+        // - The listener object
+        PendingResult<Status> regResult =
+                Fitness.SensorsApi.add(mClient,
+                        new SensorRequest.Builder()
+                                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                                .build(),
+                        listener);
+
+
+        // 4. Check the result asynchronously
+        regResult.setResultCallback(new ResultCallback<Status>()
+        {
+            @Override
+            public void onResult(Status status) {
+                if (status.isSuccess()) {
+                    Log.d(TAG, "listener registered");
+                    // listener registered
+                } else {
+                    Log.d(TAG, "listener not registered");
+                    // listener not registered
+                }
+            }
+        });
+    }
+
+    // Update the Text Viewer with Counter of Steps..
+    private void updateTextViewWithStepCounter(final int numberOfSteps) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getBaseContext(), "On Datapoint!", Toast.LENGTH_SHORT);
+
+                if(mFirstCount && (numberOfSteps != 0)) {
+                    mInitialNumberOfSteps = numberOfSteps;
+                    mFirstCount = false;
+                }
+                if(mStepsTextView != null){
+                    mStepsTextView.setText(String.valueOf(numberOfSteps - mInitialNumberOfSteps));
+                }
+            }
+        });
+    }
+
+    //Start
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirstCount = true;
+        mInitialNumberOfSteps = 0;
+        if (mClient == null || !mClient.isConnected()) {
+            connectFitness();
+        }
+    }
+    //Stop
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mClient.isConnected() || mClient.isConnecting()) mClient.disconnect();
+        mInitialNumberOfSteps = 0;
+        mFirstCount = true;
+    }
 
 }
