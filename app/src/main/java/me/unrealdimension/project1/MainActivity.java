c3b688b297642e899df57cf0a36e062e97e871ca
AeternusSamurai <darksamurai515@gmail.com>
2015-04-09 13:53:11 -0400

    Revert "Re-adding the menu stuffs"
    This reverts commit a789ad2dd9eda719ecdae8acb85cd23be6b970a4.

--- app/src/main/java/me/unrealdimension/project1/MainActivity.java
+++ app/src/main/java/me/unrealdimension/project1/MainActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -76,13 +75,6 @@ public class MainActivity extends ActionBarActivity
 
 
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
